30d5dc88886b99d02f17be9db08097afdcd91db9
samboey <samuelkjboey@gmail.com>
2019-06-24 00:01:29 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,27 @@
-const cards = ["queen", "queen", "king", "king"];
-const cardsInPlay = []
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []
 
-const cardOne = cards[0];
-cardsInPlay.push('cardOne');
-console.log("User flipped " + cardOne);
+function checkForMatch(){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+};
 
-const cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2){
+function flipcard(cardId) {
+    checkForMatch()
+    if (cardsInPlay.length === 2){
     alert("Sorry,try again.");
 }
 else if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
+} else{
+    console.log("User flipped " + cards[cardId]);
 }
+};
+
+cardsInPlay.push(cards);
+
+flipcard([0]);
+flipcard([2]);
